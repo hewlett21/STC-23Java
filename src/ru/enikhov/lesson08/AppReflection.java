d77b056a8d3e49ae1e064730097f115e8d99dc55
unknown <hewlett@mail.ru>
2020-05-31 00:37:30 +0400

    Lesson08 ready commit

--- src/ru/enikhov/lesson08/AppReflection.java
+++ src/ru/enikhov/lesson08/AppReflection.java
@@ -2,94 +2,112 @@ package ru.enikhov.lesson08;
 
 import java.io.*;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AppReflection {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        List<String> list = Arrays.asList("яблоко","груша","апельсин");
+        List<String> list = Arrays.asList("яблоко", "груша", "апельсин");
         Person tom = new Person("Tom", 35, 1.65, true, list);
+        System.out.print("Исходный объект:");
         System.out.println(tom);
         saveObj(tom);
         System.out.println("Create new object:");
         createObj();
     }
 
-    private static void createObj() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class c = Class.forName("ru.enikhov.lesson08.Person");
-        Object newTom = c.newInstance();
-        System.out.println("Пустой объект: " + newTom);
+    /** Метод заполняет поля нового объекта из файла
+     *
+     * @param newObj
+     */
+    private static void fieldSet(Object newObj) {
         try (FileInputStream fis = new FileInputStream("data.bin");
              DataInputStream dis = new DataInputStream(fis);
         ) {
-            Field name = newTom.getClass().getDeclaredField("name");
-            name.setAccessible(true);
-            name.set(newTom, dis.readUTF());
-            Field age = newTom.getClass().getDeclaredField("age");
-            age.setAccessible(true);
-            age.setInt(newTom, dis.readInt());
-            Field height = newTom.getClass().getDeclaredField("height");
-            height.setAccessible(true);
-            height.setDouble(newTom, dis.readDouble());
-            Field married = newTom.getClass().getDeclaredField("married");
-            married.setAccessible(true);
-            married.setBoolean(newTom, dis.readBoolean());
-            Field list = newTom.getClass().getDeclaredField("list");
-            list.setAccessible(true);
-//????
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+            Field[] fieldType = newObj.getClass().getDeclaredFields();
+            for (Field f : fieldType) {
+                f.setAccessible(true);
+                switch (f.getName()) {
+                    case ("name"):
+                        f.set(newObj, dis.readUTF());
+                        break;
+                    case ("age"):
+                        f.set(newObj, dis.readInt());
+                        break;
+                    case ("height"):
+                        f.set(newObj, dis.readDouble());
+                        break;
+                    case ("married"):
+                        f.set(newObj, dis.readBoolean());
+                        break;
+                    case ("list"):
+                        int sizeList = dis.readInt();
+                        List<String> actualList = new ArrayList<>();
+                        for (int i = 0; i < sizeList; i++) {
+                            actualList.add(dis.readUTF());
+                        }
+                        f.set(newObj, actualList);
+                }
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (IllegalAccessException illegalAccessException) {
+            illegalAccessException.printStackTrace();
         }
+    }
+
+    /**Создаем новый объект newTom с пустым значением полей
+     * и вызываем метод для считывания значений полей
+     *
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    private static void createObj() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class c = Class.forName("ru.enikhov.lesson08.Person");
+        Person newTom = (Person) c.newInstance();
+        System.out.println("Пустой объект: " + newTom);
+        fieldSet(newTom);
         System.out.println("Новый объект: " + newTom);
     }
 
+    /** метод saveObj записывает в файл поля класс Person
+     * для поля типа List, сначала записывается размер списка (будет использоваться
+     * при создании нового объекта),
+     * потом значения.
+      * @param obj
+     */
     private static void saveObj(Person obj) {
         Field[] fields = obj.getClass().getDeclaredFields();
-        System.out.println("----------------------");
         try (
                 FileOutputStream fos = new FileOutputStream("data.bin");
                 DataOutputStream dos = new DataOutputStream(fos)
         ) {
-
             for (Field field : fields) {
-                System.out.print(
-                        Modifier.toString(field.getModifiers()) + " " +
-                                field.getType().getSimpleName() + " " +
-                                field.getName() + ": ");
-                field.setAccessible(true); // доступ к приватному полю
-                System.out.println(field.get(obj));
-                System.out.println("========================");
+                field.setAccessible(true);
                 Object fValue = field.get(obj);
-                switch (field.getType().getSimpleName()) {
-                    case ("String"):
+                switch (field.getName()) {
+                    case ("name"):
                         dos.writeUTF(fValue.toString());
-                        System.out.println("Поле " + fValue +" записано");
                         break;
-                    case ("int"):
+                    case ("age"):
                         dos.writeInt((int) fValue);
-                        System.out.println("Поле " + fValue +" записано");
                         break;
-                    case ("double"):
+                    case ("height"):
                         dos.writeDouble((double) fValue);
-                        System.out.println("Поле " + fValue +" записано");
                         break;
-                    case ("boolean"):
+                    case ("married"):
                         dos.writeBoolean((boolean) fValue);
-                        System.out.println("Поле " + fValue +" записано");
                         break;
-                }
-                String type = field.getType().getSimpleName();
-                if (type.equals("List")) {
-                    System.out.println(field.get(obj));
-                    for (String s: obj.getList()) {
+                    case ("list"):
+                        dos.writeInt(obj.getList().size());
+                        for (String s : obj.getList()) {
                             dos.writeUTF(s);
-                    }
+                        }
                 }
             }
         } catch (IOException |
