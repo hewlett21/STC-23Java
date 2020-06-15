71e4a160dd633e6a0696c2377c30ceb24fb97662
hewlett21 <hewlett@mail.ru>
2020-06-15 23:37:05 +0300

    Lesson13 first commit

--- src/ru/enikhov/lesson12/task2/SomeClassImpl.java
+++
@@ -1,8 +0,0 @@
-package ru.enikhov.lesson12.task2;
-
-public class SomeClassImpl implements SomeInterface {
-    @Override
-    public void someMethod(String s) {
-        System.out.println("что то делаю..." + s);
-    }
-}
--- src/ru/enikhov/lesson12/task2/SomeInterface.java
+++
@@ -1,5 +0,0 @@
-package ru.enikhov.lesson12.task2;
-
-public interface SomeInterface {
-    void someMethod(String s);
-}
